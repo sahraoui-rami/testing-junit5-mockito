2e5cf8d7523c74022bbe46ffa77019cc9f05166a
ChefServiceInfo <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>
2023-12-01 14:52:33 +0100

    Testing Annotation Mocks.

---
+++ src/test/java/guru/springframework/AnnotationMocksTest.java
@@ -0,0 +1,26 @@
+package guru.springframework;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Map;
+
+/**
+ * Created by Rami SAHRAOUI on 01/12/2023
+ */
+class AnnotationMocksTest {
+    @Mock
+    Map<String, Object> mapMock;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    void testMock() {
+        mapMock.put("keyValue", "foo");
+    }
+}
