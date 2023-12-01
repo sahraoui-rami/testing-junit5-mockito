9e0feec23f78daba987a39fcbc114e1edd5011ce
ChefServiceInfo <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>
2023-12-01 17:51:57 +0100

    Returning Values From Mockito Mocks.

--- src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
+++ src/test/java/guru/springframework/sfgpetclinic/services/springdatajpa/SpecialitySDJpaServiceTest.java
@@ -8,6 +8,9 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.*;
 
 /**
@@ -62,5 +65,18 @@ class SpecialitySDJpaServiceTest {
         verify(specialtyRepository, atLeast(2)).deleteById(1L);
 
         verify(specialtyRepository, never()).deleteById(5L);
+    }
+
+    @Test
+    void findById() {
+        Speciality speciality = new Speciality();
+
+        when(specialtyRepository.findById(1L)).thenReturn(Optional.of(speciality));
+
+        Speciality foundSpeciality = service.findById(1L);
+
+        assertThat(foundSpeciality).isNotNull().isEqualTo(speciality);
+
+        verify(specialtyRepository).findById(1L);
     }
 }
